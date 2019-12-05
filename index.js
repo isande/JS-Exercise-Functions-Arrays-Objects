339b37e2d8f6c1cace46df65ae01cc9674caa3c8
Peggy Wollenhaupt <color.ui>
2019-12-04 19:17:21 -0700

    first 12 tests passing

--- index.js
+++ index.js
@@ -178,8 +178,14 @@ function getLastCarInfo(inventory) {
  * For example, if getCarInfoById is invoked with the inventory and the number 1,
  * it will return `This is a Lincoln Navigator`.
 */
-function getCarInfoById(/* code here */) {
-  /* code here */
+function getCarInfoById(inventory, id) {
+  for (let i = 0; i < (inventory.length - 1); i++) {
+    switch (inventory[i].id) {
+      case id:
+        return `This is a ${inventory[i].car_make} ${inventory[i].car_model}`;
+        break;
+    }
+  }
 }
 
 /**
